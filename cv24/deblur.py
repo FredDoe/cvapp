a1d878c17d6ca720f0c54bd7b118cd7ff663402b
Godfred Doe <godfredoe9@gmail.com>
2024-11-20 00:42:56 +0800

    set up the template for image deblurring

---
+++ cv24/deblur.py
@@ -0,0 +1,16 @@
+from cv24.utils import CV24ImageManipulator
+
+
+class Deblur24(CV24ImageManipulator):
+
+    def wiener(self):
+        pass
+
+    def rich_lucy(self):
+        pass
+
+    def deepblur(self):
+        pass
+
+    def deblurgan(self):
+        pass
